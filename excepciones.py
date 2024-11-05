234690342c67bbf5f7960ee0a98887047a11825d
Aneli <anelitj@gmail.com>
2024-11-05 14:49:29 -0600

    ejercicio de la clase

---
+++ excepciones.py
@@ -0,0 +1,9 @@
+
+try:
+    numero1=10
+    numero2=0
+    division=numero1/numero2
+    print(numero2)
+
+except ZeroDivisionError as e:
+    print(f"Estas intentando dividir entre 0, {e}")
---
+++ sepa.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from tkinter import messagebox
+
+ventana= tk.Tk()
+ventana.title("Holaaa")
+notebook=ttk.Notebook(ventana)
+
+pestaña1=tk.Frame(notebook)
+pestaña2=tk.Frame(notebook)
+pestaña3=tk.Frame(notebook)
+
+notebook.add(pestaña1, text="Pestaña1")
+notebook.add(pestaña2, text="Pestaña2")
+notebook.add(pestaña3, text="Pestaña3")
+
+notebook.pack(fill= "both", expand=True)
+
